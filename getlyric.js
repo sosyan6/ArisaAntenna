475ed2652fe8ef3faee5754654bbda33e579f5c8
sosyan6 <rmario1188@yahoo.co.jp>
2018-10-24 12:01:56 +0900

    Add files via upload

--- getlyric.js
+++ getlyric.js
@@ -1,13 +1,18 @@
-const { google } = require( 'googleapis' );
-const customsearch = google.customsearch( 'v1' );
+﻿const { google } 	= require( 'googleapis' );
+const customsearch 	= google.customsearch( 'v1' );
+const jsdom 		= require( 'jsdom' );
+const { JSDOM } 	= jsdom;
 
-async function hoge(){
+async function hoge( keyword ){
 	const res = await customsearch.cse.list( {
 		cx: '013248310813509807142:prs6wavqojw',
 		auth: 'AIzaSyBReyjNQE7HU-jyXE7I4pUReo44vH7DboA',
-		q: 'Cut.Cut.Cut.'
+		q: keyword + ' アイドルマスター'
 	} );
-	console.log( res.data.items[0].link );
+	JSDOM.fromURL( res.data.items[0].link ).then( ( dom ) => {
+		var lyric = String( dom.window.document.getElementById( 'kashi_area' ).innerHTML ).replace( /<br>/g, '\n' );
+		console.log( lyric );
+	} ).catch( console.error );
 }
 
-hoge();
+hoge( '永遠の花' );
